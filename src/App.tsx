80272e080ceef4726a1be1bac1ea6da8ca7372a4
ZelmaSedano <zvsedano@gmail.com>
2025-02-22 11:43:08 -0600

    adding info section in content div, and navbar at the bottom of the header

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 
-// import evil_curtis from './assets/ev'
+import butterfly from './assets/butterfly.jpg'
 import './App.css'
 
 function App() {
@@ -28,19 +28,45 @@ function App() {
   return (
     <>
       <div className="App">
+        {/* Header */}
         <header className={`header ${scrolling ? 'scrolled' : ''}`}>
           <div className="header-content">
-            <h1 className='header-h1'>Who the Hell is Curtis Yarvin?</h1>
-            <p className='header-p'>and why does J.D. Vance worship him?</p>
+            <a href='https://america2.news/content/files/2025/02/Evidence_Brief_-_Imminent_Neoreactionary_Threat.pdf' target="_blank">
+              <h1 className='header-h1'>Who the Hell is Curtis Yarvin?</h1>
+            </a>
+            <p className='header-p'>And why do Musk & Vance worship him?</p>
           </div>
+
+          {/* Navbar */}
+          <nav className="navbar">
+            <ul>
+              <li><a href="#info">Info</a></li>
+              <li><a href="#links">Links</a></li>
+              <li><a href="#videos">Videos</a></li>
+            </ul>
+          </nav>
         </header>
+
+        {/* bottom header with links, when you click they scroll down the page */}
       </div>
 
-      <div className="content">
-          <p>Scroll down to see the header collapse.</p>
-          {/* space so you can scroll down*/}
-          <div style={{ height: '500px' }}></div>
+      {/* Body/Content */}
+      <div className={`content ${scrolling ? 'content-scrolled' : ''}`}>
+
+        <div className='dark-enlightenment-info-section'>
+          <p className='dark-enlightenment-title'>Curtis is the co-creator of the <a className='dark-enlightenment-link' href='https://en.wikipedia.org/wiki/Dark_Enlightenment' target="_blank">"Dark Enlightenment"</a></p>
+          <p className='dark-enlightenment-text'>The Dark Enlightenment states that America's democracy has failed and it must be replaced with a "Techno-Monarchy".  That this King would be synonymous with a dictator, and that he would simply be a figurehead as a CEO runs the country like a business.</p>
+          <p className='dark-enlightenment-text'>We are witnessing the dismantling of democracy, and they are following his plan to a T.  To fight this fascist movement, we must inform ourselves on their plan and FIGHT BACK TO SAVE OUR COUNTRY.</p>
+          <p className='dark-enlightenment-text'>We have approximately 6 months to get our country back.</p>
         </div>
+
+        <div className='butterfly'>
+          <a href="https://react.dev" target="_blank">
+            <img src={butterfly} className="butterfly" alt="butterfly revolution" />
+          </a>
+        </div>
+
+      </div>
     </>
   )
 }
