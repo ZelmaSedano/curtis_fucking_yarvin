5f986f39816668fa27400e3dca8bac2e8341f3e5
ZelmaSedano <zvsedano@gmail.com>
2025-02-23 07:42:27 -0600

    adding id and updating copy

--- src/App.tsx
+++ src/App.tsx
@@ -32,17 +32,17 @@ function App() {
         <header className={`header ${scrolling ? 'scrolled' : ''}`}>
           <div className="header-content">
             <a href='https://america2.news/content/files/2025/02/Evidence_Brief_-_Imminent_Neoreactionary_Threat.pdf' target="_blank">
-              <h1 className='header-h1'>Who the Hell is Curtis Yarvin?</h1>
+              <h1 className='header-h1'>who the hell is Curtis Yarvin?</h1>
             </a>
-            <p className='header-p'>And why do Musk & Vance worship him?</p>
+            <p className='header-p'>why do Musk and Vance worship him?</p>
           </div>
 
           {/* Navbar */}
           <nav className="navbar">
             <ul>
-              <li><a href="#info">Info</a></li>
-              <li><a href="#links">Links</a></li>
-              <li><a href="#videos">Videos</a></li>
+              <li><a href="#info">info</a></li>
+              <li><a href="#links">links</a></li>
+              <li><a href="#videos">videos</a></li>
             </ul>
           </nav>
         </header>
@@ -51,20 +51,35 @@ function App() {
       </div>
 
       {/* Body/Content */}
-      <div className={`content ${scrolling ? 'content-scrolled' : ''}`}>
+      <div className='content'>
 
-        <div className='dark-enlightenment-info-section'>
-          <p className='dark-enlightenment-title'>Curtis is the co-creator of the <a className='dark-enlightenment-link' href='https://en.wikipedia.org/wiki/Dark_Enlightenment' target="_blank">"Dark Enlightenment"</a></p>
-          <p className='dark-enlightenment-text'>The Dark Enlightenment states that America's democracy has failed and it must be replaced with a "Techno-Monarchy".  That this King would be synonymous with a dictator, and that he would simply be a figurehead as a CEO runs the country like a business.</p>
-          <p className='dark-enlightenment-text'>We are witnessing the dismantling of democracy, and they are following his plan to a T.  To fight this fascist movement, we must inform ourselves on their plan and FIGHT BACK TO SAVE OUR COUNTRY.</p>
-          <p className='dark-enlightenment-text'>We have approximately 6 months to get our country back.</p>
-        </div>
+        {/* Info Section */}
+        <section id='info'>
+          <div className='dark-enlightenment-info-section'>
+            <p className='dark-enlightenment-title'>Curtis is the co-creator of the <a className='dark-enlightenment-link' href='https://en.wikipedia.org/wiki/Dark_Enlightenment' target="_blank">"Dark Enlightenment"</a></p>
+            <p className='dark-enlightenment-text'>The Dark Enlightenment states that America's democracy has failed and it must be replaced with a "Techno-Monarchy".  That this King would be synonymous with a dictator, and that he would simply be a figurehead as a CEO runs the country like a business.</p>
+            <p className='dark-enlightenment-text'>We are witnessing the dismantling of democracy, and they are following his plan to a T.  To fight this fascist movement, we must inform ourselves on their plan and FIGHT BACK TO SAVE OUR COUNTRY.</p>
+            <p className='dark-enlightenment-text'>We have approximately 6 months to get our country back.</p>
+          </div>
 
-        <div className='butterfly'>
-          <a href="https://react.dev" target="_blank">
-            <img src={butterfly} className="butterfly" alt="butterfly revolution" />
-          </a>
-        </div>
+          <div className='butterfly'>
+            <a href="https://america2.news/content/files/2025/02/Evidence_Brief_-_Imminent_Neoreactionary_Threat.pdf" target="_blank">
+              <img src={butterfly} className="butterfly" alt="butterfly revolution" />
+            </a>
+          </div>
+        </section>
+
+        {/* Links Section */}
+        <section id="links" className='links-section'>
+          <h2>Links Section</h2>
+          <p>Here are some useful links about Curtis Yarvin and the Dark Enlightenment.</p>
+        </section>
+
+        {/* Videos Section */}
+        <section id="videos" className='videos-section'>
+          <h2>Videos Section</h2>
+          <p>Here are some videos explaining Curtis Yarvin's ideas.</p>
+        </section>
 
       </div>
     </>
