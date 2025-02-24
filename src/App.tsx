7e175fe64e841a4a9cf4bf8311f8b304b1bc01ad
ZelmaSedano <zvsedano@gmail.com>
2025-02-24 11:34:20 -0600

    adding videos and links

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,16 @@ function App() {
   }, []);
   // end of block
 
+  // videos object
+  const videos1 = [
+    '5RpPTRcz1no', // Example video ID
+    'NcSil8NeQq8',
+    'mYrPNvVhKLU',
+    'rpEg4LS3CT0',
+    'rRq14ZBYwus',
+    'RRzfsbIkSoo',
+  ];
+
 
   return (
     <>
@@ -63,7 +73,9 @@ function App() {
           </div>
 
           <div className='butterfly'>
-            <a href="https://america2.news/content/files/2025/02/Evidence_Brief_-_Imminent_Neoreactionary_Threat.pdf" target="_blank">
+            <p className='butterfly-title'>These are the 7 steps of Yarvin's <a href='https://graymirror.substack.com/p/the-butterfly-revolution' target='_blank'>Butterfly Revolution</a></p>
+            <p className='butterfly-text'>We are currently on Step 4 out of 7</p>
+            <a href="https://graymirror.substack.com/p/the-butterfly-revolution" target="_blank">
               <img src={butterfly} className="butterfly" alt="butterfly revolution" />
             </a>
           </div>
@@ -71,17 +83,40 @@ function App() {
 
         {/* Links Section */}
         <section id="links" className='links-section'>
-          <h2>Links Section</h2>
-          <p>Here are some useful links about Curtis Yarvin and the Dark Enlightenment.</p>
+          <h2 className='links-title'>Links</h2>
+          <p className='links-text'>Recently there have been many articles about Yarvin cropping up in the Zeitgeist.  Here are a few.</p>
+
+          <p className='links'><a href='https://www.nytimes.com/2025/01/18/magazine/curtis-yarvin-interview.html' target='_blank'>New York Times - Curtis Yaring Says Democracy is Done.  Powerful Conservatives are Listening</a></p>
+          <p className='links'><a href='https://www.theguardian.com/us-news/2024/dec/21/curtis-yarvin-trump' target='_blank'>The Guardian - He’s anti-democracy and pro-Trump: the obscure ‘dark enlightenment’ blogger influencing the next US administration</a></p>
+          <p className='links'><a href='https://www.politico.com/news/magazine/2025/01/30/curtis-yarvins-ideas-00201552' target='_blank'>Politico - Curtis Yarvin’s Ideas Were Fringe. Now They’re Coursing Through Trump’s Washington</a></p>
+
+          <p className='links'><a href='https://america2.news/content/files/2025/02/Evidence_Brief_-_Imminent_Neoreactionary_Threat.pdf' target='_blank'>Silicon Valley Memo: The Imminent Neoreactionary Threat to the American Republic</a></p>
+          <p className='links'><a href='https://www.thenerdreich.com/reboot-elon-musk-ceo-dictator-doge/' target='_blank'>Nerd Reich: 'Reboot' Revealed: Elon Musk's CEO-Dictator Playbook</a></p>
+          <p className='links'><a href='https://www.rollingstone.com/politics/politics-features/owned-book-peter-thiel-trump-tech-silicon-valley-1235276868/' target='_blank'>Rolling Stone: Peter Thiel, Trump’s Man in Silicon Valley</a></p>
+          <p className='links'><a href='https://www.democracynow.org/2025/2/20/quinn_slobodian_maga_doge_capitalism' target='_blank'>Democracy Now: “Crack-Up Capitalism”: Historian Quinn Slobodian on Trump, Musk & the Movement to “Shatter” the State</a></p>
+
         </section>
 
         {/* Videos Section */}
         <section id="videos" className='videos-section'>
-          <h2>Videos Section</h2>
+          <h2 className='videos-title'>Videos</h2>
           <p>Here are some videos explaining Curtis Yarvin's ideas.</p>
-        </section>
 
-      </div>
+        <div className="video-grid">
+          {/* map over the videos1 object */}
+          {videos1.map((videoId, index) => (
+            <iframe
+              key={index}
+              src={`https://www.youtube.com/embed/${videoId}`}
+              title={`YouTube video ${index}`}
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+          ))}
+        </div>
+      </section>
+    </div>
     </>
   )
 }
